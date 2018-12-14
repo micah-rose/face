bca38e2ea1e6ad9f2b0b8376308b9b52fe66242e
micah-rose <micah.r.lund@gmail.com>
2018-12-14 14:56:52 -0700

    confirmed program behavior and made appropriate changes in JavaScript code. Tested program based on instructions and functionality works as expected. Removed Java file for finalization

--- js/face.js
+++ js/face.js
@@ -1,25 +1,51 @@
-//TO DO's:
-//Create random variables (will need 3)
-//Add functions for check boxes to change corresponding images
 
-var rand;
-var rand2;
-var rand3;
-
-rand = Math.ceil(Math.random()*2);
-
-function eyesCheck(){
-
-}
-
-function noseCheck(){
-
-}
-
-function mouthCheck(){
-
-}
+var numFace = 0;
+var numEye = 0;
+var numNose = 0;
+var numMouth = 0;
 
 function changeFace(){
+    checkbox1 = document.getElementById("chkbx1");
+    checkbox2 = document.getElementById("chkbx2");
+    checkbox3 = document.getElementById("chkbx3");
+      
+        if(checkbox1.checked){
+            if (numEye != 2){
+	            numEye++;
+            }else 
+            {
+	            numEye = 0;
+            }
+           document.getElementById("eyes-image").src = "img/eyes" + numEye + ".png";
+        }
+        if(checkbox2.checked){
+            if (numNose != 2){
+	            numNose++;
+            }else 
+            {
+	            numNose = 0;
+            }
+            document.getElementById("nose-image").src = "img/nose" + numNose + ".png";      
+        }
+        if(checkbox3.checked){
+            if (numMouth != 2){
+	            numMouth++;
+            }else 
+            {
+	            numMouth = 0;
+            }
+            document.getElementById("mouth-image").src = "img/mouth" + numMouth + ".png";    
+        }
 
-}
+        if (!(checkbox1.checked) && !(checkbox2.checked) && !(checkbox3.checked)) 
+        {
+            if (numFace != 2) 
+            {
+                numFace++;
+            } else 
+            {
+                numFace = 0;
+            }
+            document.getElementById("face-image").src = "img/face" + numFace + ".png";
+        }
+    }
