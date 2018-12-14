addc9d380f36d7a5745b4c10edcaef127cbc9ca4
micah-rose <micah.r.lund@gmail.com>
2018-12-14 12:49:31 -0700

    finalized the HTML and CSS for this. Next step is to complete the JavaScript functionality, but will need to confirm program behavior as it is written with Java before starting

---
+++ js/face.js
@@ -0,0 +1,25 @@
+//TO DO's:
+//Create random variables (will need 3)
+//Add functions for check boxes to change corresponding images
+
+var rand;
+var rand2;
+var rand3;
+
+rand = Math.ceil(Math.random()*2);
+
+function eyesCheck(){
+
+}
+
+function noseCheck(){
+
+}
+
+function mouthCheck(){
+
+}
+
+function changeFace(){
+
+}
